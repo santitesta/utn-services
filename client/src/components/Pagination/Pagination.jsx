be0282ddb6d05476231883c86b2c94d307de62c7
Santiago Andrés Testa <testasanty@gmail.com>
2022-05-26 14:11:44 -0300

    Horrible pagination v2

--- client/src/components/Pagination/Pagination.jsx
+++ client/src/components/Pagination/Pagination.jsx
@@ -2,31 +2,31 @@ import style from './Pagination.module.css'
 
 function Pagination({ currentPage, productsPerPage, totalProducts, paginate }) {
 
-    const pageNumbers = [];
-    for (let i = 1; i <= Math.ceil(totalProducts / productsPerPage); i++) {
-        pageNumbers.push(i);
-    };
-    return (
-        <nav className={style.container}>
-            {/* <ul className={style.pageList}> */}
-                {pageNumbers.map(number => {
-                    return (
-                        <li
-                            key={number}
-                            className={style.items}
-                        >
-                            <a
-                                onClick={() => paginate(number)}
-                                href='#top'
-                                className={currentPage===number?style.pageSel:style.pages}
-                            >{number}
-                            </a>
-                        </li>
-                    )
-                })}
-            {/* </ul> */}
-        </nav>
-    )
+		const pageNumbers = [];
+		for (let i = 1; i <= Math.ceil(totalProducts / productsPerPage); i++) {
+				pageNumbers.push(i);
+		};
+		return (
+				<nav className={style.container}>
+						{/* <ul className={style.pageList}> */}
+								{pageNumbers.map(number => {
+										return (
+												<li
+														key={number}
+														className={style.items}
+												>
+														<a
+																onClick={() => paginate(number)}
+																href='#top'
+																className={currentPage===number?style.pageSel:style.pages}
+														>{number}
+														</a>
+												</li>
+										)
+								})}
+						{/* </ul> */}
+				</nav>
+		)
 }
 
 export default Pagination;
