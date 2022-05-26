208631158a122464642c21d6c850226a824e9233
Santiago Andrés Testa <testasanty@gmail.com>
2022-05-26 14:47:49 -0300

    Clean start!

--- api2/src/db.js
+++ api2/src/db.js
@@ -6,6 +6,8 @@ const sequelize = new Sequelize('utn','postgres','153624San.',{
   logging: false
 })
 
+const Models = []
+
 const utnModel = sequelize.define('equipos', {
   "id": {type: Sequelize.INTEGER, primaryKey: true},
   "equipo": Sequelize.INTEGER
@@ -21,3 +23,16 @@ module.exports = {
   utnModel, // para poder importar los modelos así: const { Product, User } = require('./db.js');
   conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
 };
+
+// const utnModel = sequelize.define('Devices', {
+//   "id_inei": {type: Sequelize.INTEGER, primaryKey: true},
+//   "Instituto": Sequelize.INTEGER,
+//   "Departamento": Sequelize.VARCHAR,
+//   "Servicio": Sequelize.VARCHAR,
+//   "equipo": Sequelize.VARCHAR,
+//   "e_tecnico": Sequelize.VARCHAR,
+//   "marca": Sequelize.VARCHAR,
+//   "modelo": Sequelize.VARCHAR
+//   },
+//   {timestamps: false}
+// );
