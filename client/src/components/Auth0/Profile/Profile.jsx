38ba06ed2fc75afc7c08642a13b4fd4a8fcb1e69
Santiago Andrés Testa <testasanty@gmail.com>
2022-05-27 11:53:09 -0300

    CSS(Minimal fixes to css)

--- client/src/components/Auth0/Profile/Profile.jsx
+++ client/src/components/Auth0/Profile/Profile.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import styles from './Profile.module.css'
 
 const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
@@ -10,11 +11,13 @@ const Profile = () => {
 
   return (
     isAuthenticated?(
-      <div>
+      <div className={styles.container}>
         <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
-        {user.name === 'Santiago Testa'?<h1>wuachin</h1>:<h1>no wuachin</h1>}
+        <h2 className={styles.username}>{user.name}</h2>
+        <p className={styles.mail}>{user.email}</p>
+        {user.name === 'Santiago Testa'?
+        <h1 className={styles.type}>Admin</h1>
+        :<h1 className={styles.type}>User</h1>}
       </div>
     )
     :<h1>Nadie logeado</h1>
