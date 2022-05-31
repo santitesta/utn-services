73971867842e415a0688b98ace365077dab2b230
UTN <utn@ANLIS2>
2022-05-31 15:23:12 -0300

    Feat(Environment variables)

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,11 +1,16 @@
 import axios from "axios"
 
+let url = 'http://localhost:3001'
+if(process.env.STAGE !== "development") {
+	url = 'http://10.10.200.125:3001'
+}
+
 export const GET_EQUIPOS = "GET_EQUIPOS"
 export const GET_DEVICE_BY_ID = "GET_DEVICE_BY_ID"
 
 export const getEquipos = () => {
 	return function(dispatch){
-		return axios.get(`http://localhost:3001/equipos`)
+		return axios.get(`${url}/equipos`)
 			.then(resp => dispatch({type: GET_EQUIPOS, payload: resp.data}))
 			.catch(error => alert('Error in getRecipes: ',error))
 	}
@@ -13,7 +18,7 @@ export const getEquipos = () => {
 
 export const getDeviceById = (id) => {
 	return function(dispatch){
-		return axios.get(`http://localhost:3001/equipos/${id}`)
+		return axios.get(`${url}/equipos/${id}`)
 			.then(resp => dispatch({type: GET_DEVICE_BY_ID, payload: resp.data}))
 			.catch(error => alert('Error in getRecipes: ',error))
 	}
