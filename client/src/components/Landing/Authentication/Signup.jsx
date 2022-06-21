382df54660a93fdf436afbb330c5ed918bcc6b65
santitesta <testasanty@gmail.com>
2022-06-21 13:26:25 -0300

    CSS

--- client/src/components/Landing/Authentication/Signup.jsx
+++ client/src/components/Landing/Authentication/Signup.jsx
@@ -44,7 +44,7 @@ export default function Signup() {
   }
 
   return (
-    <div className='border-solid border-2 border-sky-500 p-2 grid place-items-center'>
+    <div className='border-solid border-2 border-sky-500 p-2 grid place-items-center gap-1'>
 
       <input className="mt-1 block w-60 px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400focus:outline-none
           focus:border-sky-500 focus:ring-1 focus:ring-sky-500 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none "
@@ -55,13 +55,15 @@ export default function Signup() {
         type="password" placeholder='Password'
         onChange={signUpChange} name='password' value={userSignup.password} />
 
-      {institutes.map(i => {
-        return <div key={i}>
-          <input type='radio' id={i} name='institute' value={i} onChange={signUpChange} />
-          <label htmlFor={i}>{i}</label>
-          <br />
-        </div>
-      })}
+      <div className='w-56 grid grid-cols-1 gap-1'>
+        {institutes.map(i => {
+          return <div key={i}>
+            <input type='radio' id={i} name='institute' value={i} onChange={signUpChange} />
+            <label htmlFor={i}>{i}</label>
+            <br />
+          </div>
+        })}
+      </div>
 
       {Object.keys(errors).length ? <p>{errors}</p> : null}
 
