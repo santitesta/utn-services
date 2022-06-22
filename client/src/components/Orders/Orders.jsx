5a6277000f656fa3b7489b4282fb021c779a2ab9
santitesta <testasanty@gmail.com>
2022-06-22 13:33:17 -0300

    Feat(Create and show orders)

--- client/src/components/Orders/Orders.jsx
+++ client/src/components/Orders/Orders.jsx
@@ -1,20 +1,27 @@
 import React from 'react'
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useForm } from 'react-hook-form';
-import { createOrder } from '../../redux/actions';
+import { createOrder, getOrders } from '../../redux/actions';
+import { useEffect } from 'react';
 
 const Orders = () => {
   const dispatch = useDispatch()
   const { register, handleSubmit, reset } = useForm();
+  let orders = useSelector(state => state.orders)
+  console.log('las ordenes tigre: ', orders)
+
+  useEffect(()=>{
+    dispatch(getOrders())
+  }, [])
 
   const onSubmit = async data => {
-    console.log(data)
-    dispatch(createOrder({
+    await dispatch(createOrder({
       id_inei: data.id_inei,
       motive: data.motive,
       commentary: data.commentary
     }))
     reset()
+    dispatch(getOrders())
   };
 
   return (
@@ -26,6 +33,13 @@ const Orders = () => {
         <input type="text" placeholder='Comentario...' {...register("commentary")} />
         <input type="submit" value='Crear orden' className='m-1' />
       </form>
+      {orders ?
+        orders.map(o => {
+          return <div className='bg-slate-500'>
+            <h1>{o.commentary}</h1>
+          </div>
+        })
+        : <h1>No existen ordenes cargadas</h1>}
     </div>
   )
 }
