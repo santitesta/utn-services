c11ea25f8c26af49be500c704771208b7f49a8b0
santitesta <testasanty@gmail.com>
2022-06-24 14:51:30 -0300

    CSS(NavBar avatar)

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -65,15 +65,15 @@ export default function NavBarBro() {
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
 
                 {/* Profile dropdown */}
-                <Menu as="div" className="ml-3 relative z-50">
+                <Menu as="div" className="mt-3 ml-3 relative z-50">
                   <div>
                     <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
                       <span className="sr-only">Open user menu</span>
-                      <img
-                        className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
-                      />
+                      <div class="avatar placeholder">
+                        <div class="bg-neutral-focus text-neutral-content rounded-full w-12">
+                          <span>{localStorage.user ? localStorage.user.slice(0,2).toUpperCase() : '-'}</span>
+                        </div>
+                      </div>
                     </Menu.Button>
                   </div>
                   <Transition
