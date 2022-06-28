d08957bf6d0156f39d2c05926a95700c58656b87
santitesta <testasanty@gmail.com>
2022-06-28 11:39:37 -0300

    Translate(Navbar)

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -14,14 +14,16 @@ export default function NavBarBro() {
   const dispatch = useDispatch();
 
   let navigation = [
-    { name: 'Landing', href: '/' },
-    { name: 'Home', href: '/home' },
-    { name: 'About', href: '/about' },
+    { name: 'Ingresar', href: '/' },
+    { name: 'Inicio', href: '/home' },
+    { name: 'Sobre nosotros', href: '/about' },
   ]
 
   if (localStorage.institute === 'Admin') {
-    navigation.push({ name: 'Admin', href: '/admin' })
+    navigation.shift()
+    navigation.push({ name: 'Panel de Admin', href: '/admin' })
   } else if (localStorage.institute) {
+    navigation.shift()
     navigation.push({ name: 'Crear orden', href: '/order' })
   }
 
@@ -71,7 +73,7 @@ export default function NavBarBro() {
                       <span className="sr-only">Open user menu</span>
                       <div class="avatar placeholder">
                         <div class="bg-neutral-focus text-neutral-content rounded-full w-12">
-                          <span>{localStorage.user ? localStorage.user.slice(0,2).toUpperCase() : '-'}</span>
+                          <span>{localStorage.user ? localStorage.user.slice(0, 2).toUpperCase() : '-'}</span>
                         </div>
                       </div>
                     </Menu.Button>
@@ -86,20 +88,6 @@ export default function NavBarBro() {
                     leaveTo="transform opacity-0 scale-95"
                   >
                     <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <button className={classNames(active ? 'bg-gray-100' : '', 'block w-full px-4 py-2 text-sm text-gray-700')}>
-                            Your Profile
-                          </button>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <button className={classNames(active ? 'bg-gray-100' : '', 'block w-full px-4 py-2 text-sm text-gray-700')}>
-                            Settings
-                          </button>
-                        )}
-                      </Menu.Item>
                       <Menu.Item>
                         {({ active }) => (
                           <button className={classNames(active ? 'bg-gray-100' : '', 'block w-full px-4 py-2 text-sm text-gray-700')}
