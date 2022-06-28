59803fbe3726cd61dc7c11acc888ce114f162518
santitesta <testasanty@gmail.com>
2022-06-28 13:23:14 -0300

    CSS(Advices of permissions)

--- client/src/components/AdminSite/AdminSite.jsx
+++ client/src/components/AdminSite/AdminSite.jsx
@@ -17,12 +17,12 @@ export default function AdminPage() {
   if (verified === 'false') {
     return (
       <>
-        <h1>
-          Pida al Admin General permisos para operar como Administrador
-        </h1>
-        <h3>
-          Cuando te habiliten, cierra sesión e ingresa nuevamente con esta cuenta para tener acceso completo
-        </h3>
+        <div class="alert shadow-lg w-2/5 ml-3 mt-3">
+          <div>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-info flex-shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+            <span>Pide al Admin General permisos para operar como Administrador. Cuando te habiliten, cierra sesión e ingresa nuevamente con esta cuenta para tener acceso completo</span>
+          </div>
+        </div>
       </>
     )
   }
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -11,12 +11,12 @@ function Home() {
   if (verified === 'false') {
     return (
       <>
-        <h1>
-          El equipo de UTN te dejará entrar pronto!
-        </h1>
-        <h3>
-          Cuando te habiliten, cierra sesión e ingresa nuevamente con esta cuenta para tener acceso completo
-        </h3>
+        <div class="alert shadow-lg w-2/5 ml-3 mt-3">
+          <div>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-info flex-shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+            <span>El equipo de UTN te dejará entrar pronto! Cuando te habiliten, cierra sesión e ingresa nuevamente con esta cuenta para tener acceso completo</span>
+          </div>
+        </div>
       </>
     )
   }
@@ -31,9 +31,12 @@ function Home() {
   }
 
   return (
-    <h1>
-      Ingrese con su cuenta para ver sus equipos y crear órdenes
-    </h1>
+    <div class="alert shadow-lg w-2/5 ml-3 mt-3">
+      <div>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-info flex-shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+        <span>Ingrese con su cuenta para ver sus equipos y crear órdenes</span>
+      </div>
+    </div>
   )
 };
 
