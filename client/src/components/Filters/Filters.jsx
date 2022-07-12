ffe78d5c0f794d9d08a7a0227f81a8fe18e3dd01
santitesta <testasanty@gmail.com>
2022-07-12 14:17:17 -0300

    Feat(Services filter showing yet not working)

--- client/src/components/Filters/Filters.jsx
+++ client/src/components/Filters/Filters.jsx
@@ -11,6 +11,9 @@ function Filters() {
   const { register, handleSubmit, watch, reset } = useForm();
 
   const onSubmit = async data => {
+    // if(data.servicio) {
+    //   dispatch(getDevicesByService)
+    // }
     if (localStorage.institute !== 'Admin') data.instituto = localStorage.institute
     if (data.id_inei.length) {
       if (localStorage.institute === 'Admin') data.instituto = localStorage.institute
@@ -21,30 +24,66 @@ function Filters() {
       reset()
     } else alert('Antes elija algún filtro');
   };
-  console.log('Esto: ',watch("instituto") === institutes.INEI)
+  console.log('Esto: ', watch("instituto") === institutes.INEI)
 
   return (
     <div className='w-1/5 grid justify-items-center content-start'>
+
       {localStorage.institute === 'Admin' ?
+
         <form onSubmit={handleSubmit(onSubmit)} className='mt-3 grid justify-items-center content-start'>
-          <input type="number" placeholder='Buscar por Nº UTN...' {...register("id_inei")} className="input input-bordered input-accent m-1 w-44 max-w-xs" disabled={watch("instituto")} />
-          <select {...register("instituto")} className='select select-accent m-1 w-44 max-w-xs' disabled={watch("id_inei")}>
-            <option defaultValue value="">Elija Instituto...</option>
+
+          <input type="number"
+            className="input input-bordered input-accent m-1 w-44 max-w-xs"
+            placeholder='Nº UTN...'
+            {...register("id_inei")}
+            disabled={watch("instituto")} />
+
+          <select
+            className='select select-accent m-1 w-44 max-w-xs'
+            {...register("instituto")}
+            disabled={watch("id_inei")}>
+
+            <option defaultValue value="">Instituto...</option>
             {Object.keys(institutes).map(i => {
-              return <option value={institutes[i]}>{institutes[i]}</option>
+              return <option key={i} value={institutes[i]}>{institutes[i]}</option>
             })}
           </select>
-          <input type="number" placeholder={watch("instituto") === institutes.INEI ? 'Algo' : 'Algo más...'} {...register("id_inei")} className="input input-bordered input-accent m-1 w-44 max-w-xs" disabled={watch("instituto")} />
+
+          <select
+            className='select select-accent m-1 w-44 max-w-xs'
+            {...register("servicio")}
+            disabled={!watch("instituto") || !services[watch("instituto")].length}>
+
+            <option defaultValue value="">Servicio...</option>
+            {watch("instituto") && services[watch("instituto")].map(s => {
+              return <option value={s}>{s}</option>
+            })}
+          </select>
+
           <input type="submit" value='Buscar' className='btn btn-secondary m-1' />
         </form>
+
         : localStorage.institute ?
           <div className='mt-3 grid justify-items-center content-start'>
             <div className='badge badge-primary p-6 text-4xl'>{localStorage.institute}</div>
+
             <form onSubmit={handleSubmit(onSubmit)} className='mt-3 grid justify-items-center content-start'>
-              <input type="number" placeholder='Nº UTN...' {...register("id_inei")} className='input input-bordered input-accent m-1 w-44 max-w-xs' disabled={watch("instituto")} />
-              <input type="submit" value='Buscar por id o todos' className='m-1 btn btn-secondary text-sm' />
+
+              <input type="number"
+                className='input input-bordered input-accent m-1 w-44 max-w-xs'
+                placeholder='Nº UTN...'
+                {...register("id_inei")}
+                disabled={watch("instituto")} />
+
+              <input type="submit"
+                className='m-1 btn btn-secondary text-sm'
+                value='Buscar por id o todos' />
+
             </form>
+
           </div>
+
           : <h1>Debe estar logeado para ver equipos</h1>
       }
     </div>
