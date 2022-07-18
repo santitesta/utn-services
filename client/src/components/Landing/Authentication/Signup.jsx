c465fa21361e332fb84259068a2705ae2a961898
santitesta <testasanty@gmail.com>
2022-07-18 13:54:32 -0300

    Feat(Signup - Useform with errors)

--- client/src/components/Landing/Authentication/Signup.jsx
+++ client/src/components/Landing/Authentication/Signup.jsx
@@ -8,8 +8,9 @@ import { useForm } from 'react-hook-form';
 export default function Signup() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const { register, handleSubmit, watch, reset, resetField } = useForm();
+  const { register, handleSubmit, watch, resetField, formState: { errors } } = useForm();
 
+  console.log('Errors: ', errors)
   const onSubmit = async data => {
     await dispatch(signUp(data))
     if (localStorage.user) {
@@ -22,25 +23,32 @@ export default function Signup() {
       className='border-solid border-2 border-sky-500 p-2 grid place-items-center gap-1'>
 
       <input type="text"
-        name='email'
+        name='nickname'
         className="mt-1 block w-60 px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none
         focus:border-sky-500 focus:ring-1 focus:ring-sky-500 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none "
         placeholder='Nombre de usuario'
-        {...register("nickname")} />
+        {...register("nickname", { required: 'Nombre de usuario requerido' })}
+      />
 
       <input type="text"
         name='email'
         className="mt-1 block w-60 px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none
         focus:border-sky-500 focus:ring-1 focus:ring-sky-500 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none "
         placeholder='Email'
-        {...register("email")} />
+        {...register("email", {
+          required: 'Email requerido',
+          pattern: {
+            value: /\S+@\S+\.\S+/,
+            message: 'Email inválido'
+          }
+        })} />
 
       <input type="password"
         name='password'
         className="mt-1 block w-60 px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none
         focus:border-sky-500 focus:ring-1 focus:ring-sky-500 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none"
         placeholder='Contraseña'
-        {...register("password")} />
+        {...register("password", { required: 'Contraseña requerida' })} />
 
       <div className='w-56 mt-3 grid grid-cols-1 gap-1'>
 
@@ -87,14 +95,18 @@ export default function Signup() {
         value="Crear cuenta"
         className="btn btn-primary w-4/5" />
 
-      {/* {Object.keys(errors).length ?
+      {Object.keys(errors).length ?
         <div class="alert alert-warning shadow-lg">
           <div>
             <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
-            <span>{errors}</span>
+            <div className='flex flex-col justify-center'>
+              <p>{errors.nickname?.message}</p>
+              <p>{errors.email?.message}</p>
+              <p>{errors.password?.message}</p>
+            </div>
           </div>
         </div>
-        : null} */}
+        : null}
 
     </form >
   );
