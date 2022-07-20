6c40407f0f7c772ee5da43e66fd93112f1833307
Martin <cogliatti.martin@gmail.com>
2022-07-20 14:11:37 -0300

    CSS(Landing - complete remodel based on a hero)

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,65 +1,144 @@
-import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
-import styles from './Landing.module.css'
-import { useDispatch } from 'react-redux';
-import { logout } from '../../redux/actions';
-import Login from './Authentication/Login';
-import Signup from './Authentication/Signup';
+/* This example requires Tailwind CSS v2.0+ */
+import { Fragment } from 'react'
+import { Popover, Transition } from '@headlessui/react'
+import { MenuIcon, XIcon } from '@heroicons/react/outline'
 
-function Landing() {
-  const dispatch = useDispatch();
+const navigation = [
+  { name: 'Home', href: '#' },
+  { name: 'Proyectos', href: '#' },
+  { name: 'Servicios', href: '#' },
+  { name: 'Contacto', href: '#' },
+  { name: 'Nosotros', href: '#' },
+]
 
-  const [sign, setSign] = useState('')
-
-  function handleLogout(e) {
-    e.preventDefault()
-    dispatch(logout())
-  }
-
-  function showLogin() {
-    setSign('login')
-  }
-
-  function showSignup() {
-    setSign('signup')
-  }
-
+export default function Example() {
   return (
-    <>
-      {sign === '' &&
-        <div className={styles.container}>
-          <img src={require('./logo512.png')} alt="" className='w-40 h-40'/>
-          <h1 className={styles.title}>Seguimiento UTN</h1>
-          <NavLink className={styles.linkHome} to='/home'>Ingresar</NavLink>
-          <div className={styles.auth}>
-            <div className='flex justify-center gap-10'>
-              {!localStorage.getItem("user") ?
-                <>
-                  <button onClick={showLogin} className='btn btn-primary w-44'>Login</button>
-                  <button onClick={showSignup} className='btn btn-secondary w-44'>Crear cuenta</button>
-                </>
-                : <button className="btn btn-primary" onClick={handleLogout}>
-                  Logout
-                </button>
-              }
-            </div>
-          </div>
-        </div>
-      }
-      {sign === 'login' &&
-        <div className={styles.box}>
-          <Login />
-          <button onClick={() => setSign('')} className='btn btn-secondary'>Volver</button>
-        </div>
-      }
-      {sign === 'signup' &&
-        <div className={styles.box}>
-          <Signup />
-          <button onClick={() => setSign('')} className='btn btn-secondary'>Volver</button>
-        </div>
-      }
-    </>
-  );
-};
+    <div className="relative bg-white overflow-hidden">
+      <div className="max-w-7xl mx-auto">
+        <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+          <svg
+            className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
+            fill="currentColor"
+            viewBox="0 0 100 100"
+            preserveAspectRatio="none"
+            aria-hidden="true"
+          >
+            <polygon points="50,0 100,0 50,100 0,100" />
+          </svg>
 
-export default Landing;
+          <Popover>
+            <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
+              <nav className="relative flex items-center justify-between sm:h-10 lg:justify-start" aria-label="Global">
+                <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
+                  <div className="flex items-center justify-between w-full md:w-auto">
+                    <a href="#">
+                      <span className="sr-only">Workflow</span>
+                      <img
+                        alt="Workflow"
+                        className="h-8 w-auto sm:h-10"
+                        src={require("./icon_wp.png")} 
+                      />
+                    </a>
+                    <div className="-mr-2 flex items-center md:hidden">
+                      <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                        <span className="sr-only">Abrir menú</span>
+                        <MenuIcon className="h-6 w-6" aria-hidden="true" />
+                      </Popover.Button>
+                    </div>
+                  </div>
+                </div>
+                <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
+                  {navigation.map((item) => (
+                    <a key={item.name} href={item.href} className="font-medium text-gray-500 hover:text-gray-900">
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+              </nav>
+            </div>
+
+            <Transition
+              as={Fragment}
+              enter="duration-150 ease-out"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="duration-100 ease-in"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Popover.Panel
+                focus
+                className="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
+              >
+                <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
+                  <div className="px-5 pt-4 flex items-center justify-between">
+                    <div>
+                      <img
+                        className="h-8 w-auto"
+                        src={require("./icon_wp.png")} 
+                        alt=""
+                      />
+                    </div>
+                    <div className="-mr-2">
+                      <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                        <span className="sr-only">Cerrar menú</span>
+                        <XIcon className="h-6 w-6" aria-hidden="true" />
+                      </Popover.Button>
+                    </div>
+                  </div>
+                  <div className="px-2 pt-2 pb-3 space-y-1">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </Popover.Panel>
+            </Transition>
+          </Popover>
+
+          <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+            <div className="sm:text-center lg:text-left">
+              <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+                <span className="block xl:inline">UTN - Servicios</span>{' '}
+                <span className="block text-indigo-600 xl:inline">Seguimiento</span>
+              </h1>
+              <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                Por los servicios prestados en el marco del convenio firmado con ANLIS.
+              </p>
+              <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+                <div className="rounded-md shadow">
+                  <a
+                    href="#"
+                    className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+                  >
+                    Iniciar Sesión
+                  </a>
+                </div>
+                <div className="mt-3 sm:mt-0 sm:ml-3">
+                  <a
+                    href="#"
+                    className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
+                  >
+                    Registrarse
+                  </a>
+                </div>
+              </div>
+            </div>
+          </main>
+        </div>
+      </div>
+      <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+      <video height="256" loop="true" autoplay="autoplay" id="vid" muted className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"> 
+      <source src ={require("./video.mp4")}  type="video/mp4" />
+      Your browser does not support the video tag.
+     </video>
+      </div>
+    </div>
+  )
+}
