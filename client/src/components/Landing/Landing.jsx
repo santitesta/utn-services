541e35f881e84166ad25c7fefa2c7459fca822dc
Martin <cogliatti.martin@gmail.com>
2022-07-22 11:15:51 -0300

    CCS(Landing - fix video bg)

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -16,7 +16,7 @@ const navigation = [
 export default function Landing() {
   return (
     <div className="relative bg-white overflow-hidden lg:h-screen ">
-      <div className="max-w-7xl mx-auto bg-black">
+      <div className="max-w-7xl mx-auto bg-white">
         <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32 lg:h-screen">
           <svg
             className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
