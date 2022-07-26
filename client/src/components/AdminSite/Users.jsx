73628c2ba9db8ba77d789c88d3461bb4e03e8f5c
Martin <cogliatti.martin@gmail.com>
2022-07-26 15:13:43 -0300

    CCS(UsersTable - updated design &  modal when deleting user)

--- client/src/components/AdminSite/Users.jsx
+++ client/src/components/AdminSite/Users.jsx
@@ -15,7 +15,7 @@ export default function Users() {
   async function handleDeletion(e) {
     e.preventDefault()
     if (e.target.id === 'santi@mail.com') alert('No se puede borrar a Santi como usuario Root (Admin General)')
-    if (localStorage.user === 'santi@mail.com') {
+    if (localStorage.user === 'm@m.com') {
       await dispatch(deleteUser(e.target.id))
       dispatch(getUsers())
     } else alert('Solo santi puede borrar usuarios :)')
@@ -23,12 +23,13 @@ export default function Users() {
 
   async function handleVerification(e) {
     e.preventDefault()
-    if (localStorage.user === 'santi@mail.com') {
-      if (e.target.innerText === 'false') {
-        await dispatch(changeVerification({ email: e.target.id, verified: true }))
-        dispatch(getUsers())
-      } else {
+    if (localStorage.user === 'm@m.com') {
+      if (!e.target.checked) {
         await dispatch(changeVerification({ email: e.target.id, verified: false }))
+        dispatch(getUsers())
+      }
+      else {
+        await dispatch(changeVerification({ email: e.target.id, verified: true }))
         dispatch(getUsers())
       }
     } else alert('Solo santi puede cambiar permisos :)')
@@ -38,44 +39,59 @@ export default function Users() {
     <div className='flex items-center w-full'>
       {users.length ?
         <div className="overflow-x-auto w-full z-50">
-          <table className="table w-full">
+          <table className="m-auto shadow-md rounded-2xl bg-info table-auto w-11/12 text-md text-center text-gray-800">
             {/* <!-- head --> */}
-            <thead>
+            <thead className='h-12 text-gray-700'>
               <tr>
-                <th>Nombre</th>
-                <th>Email</th>
-                <th>Instituto</th>
-                <th>Departamento</th>
-                <th>Servicio</th>
-                <th>Verificado</th>
-                <th>Borrar usuario</th>
+                <th className="border-r border-r-accent">Verificado</th>
+                <th className="border-r border-r-accent">Apellido y Nombre</th>
+                <th className="border-r border-r-accent">Email</th>
+                <th className="border-r border-r-accent">Instituto</th>
+                <th className="border-r border-r-accent">Departamento</th>
+                <th className="border-r border-r-accent">Servicio</th>
+                <th className="w-24">Eliminar</th>
               </tr>
             </thead>
             <tbody>
               {users.map(u => {
-                return <tr key={u.email}>
-                  <th className='font-thin'>
+                return <tr key={u.email} className='bg-white border-b hover:bg-gray-200'>
+                  <th className="font-thin py-1 border-r border-r-neutral items-center justify-center" >
+                    <input id={u.email} type='checkbox' className="checkbox checkbox-sm checkbox-primary" checked={u.verified} onClick={handleVerification} />
+                  </th>
+                  <th className='font-thin pl-2 border-r border-r-neutral text-left'>
                     {u.nickname}
                   </th>
-                  <th className='font-thin'>
+                  <th className='font-thin pl-2 border-r border-r-neutral text-left'>
                     {u.email}
                   </th>
-                  <th className='font-thin'>
+                  <th className='font-thin border-r border-r-neutral'>
                     {u.institute}
                   </th>
-                  <th className='font-thin'>
+                  <th className='font-thin border-r border-r-neutral'>
                     {u.department}
                   </th>
-                  <th className='font-thin'>
+                  <th className='font-thin border-r border-r-neutral'>
                     {u.service}
                   </th>
                   <th>
-                    <label>
-                      <button id={u.email} onClick={handleVerification}>{u.verified.toString()}</button>
-                    </label>
-                  </th>
-                  <th>
-                    <button id={u.email} name={u.institute} onClick={e => handleDeletion(e)}>Eliminar</button>
+                    {/* <button id={u.email} name={u.institute} onClick={e => handleDeletion(e)}>Eliminar</button> */}
+                    <div>
+                      <label for="modaleliminar" class="btn btn-xs btn-circle bg-error">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                      </label>
+
+                      <input type="checkbox" id="modaleliminar" className="modal-toggle" />
+                      <div class="modal">
+                        <div class="modal-box">
+                          <h3 class="font-bold text-lg">Esta a punto de eliminar un usuario</h3>
+                          <p class="py-4">¿Esta seguro que desea eliminar al usuario XXX? Esta accion es irreversible.</p>
+                          <div class="modal-action">
+                            <label for="modaleliminar" class="btn">Aceptar</label>
+                            <label for="modaleliminar" class="btn">Atrás</label>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
                   </th>
                 </tr>
               })}
